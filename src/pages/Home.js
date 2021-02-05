f79d4090b11af7b94a3832aa906ccdab43d8961f
Rene Morales <renemorales.developer@gmail.com>
2021-02-04 22:34:37 -0600

    Added a pages directory

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from '../components/Hero';
+
+const Home = () => {
+    return (
+        <Hero hero="defaultHero">
+            Home Page
+        </Hero>
+    )
+}
+
+export default Home
