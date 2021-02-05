805b97d4e4d6fd905c42172c21dd1ed77aaf61f6
Rene Morales <renemorales.developer@gmail.com>
2021-02-05 15:25:56 -0600

    Added a Banner and a button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
 import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
 
 const Home = () => {
     return (
         <Hero hero="defaultHero">
-            Home Page
+            <Banner title="PARADISE PERFECT" subtitle="You Truly Can Have It All At Our All Inclusive Resort">
+                <Link to='/rooms' className="btn-primary">
+                    Check Our Rooms
+                </Link>    
+            </Banner>
         </Hero>
     )
 }
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Hero from '../components/Hero';
+import {Link} from 'react-router-dom'
+import Banner from '../components/Banner';
 
 const Rooms = () => {
     return (
-        <Hero hero="roomsHero" />
+        <Hero hero="roomsHero">
+            <Banner title="Our Rooms">
+                <Link to="/" className="btn-primary">
+                    Return Home
+                </Link>
+            </Banner>
+
+        </Hero>
     )
 }
 
-export default Rooms
+export default Rooms;
