8fb1535e53f0d56eb2581b94901a6e9cabd526a4
Rene Morales <renemorales.developer@gmail.com>
2021-02-05 15:10:50 -0600

    Added a Banner with an error message and a button to go back to home page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import Hero from '../components/Hero';
-
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
 
 export default function Error() {
     return (
-        <Hero/>
+        <Hero>
+            <Banner title="Page Not Found" subtitle="Sorry, but the page you are looking for might have moved or you might have mistyped the address. Check the URL and try again.">
+                <Link to="/" className="btn-primary">
+                    Return Home
+                </Link>
+            </Banner>
+        </Hero>
     )
 }
